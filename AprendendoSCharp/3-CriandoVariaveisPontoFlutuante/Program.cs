22fe690e333e140e9f255137f3ca207ec47b7e22
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-01-17 22:17:56 -0200

    A3 criando variaveis double

---
+++ AprendendoSCharp/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto  3 Criando variaveis ponto flutuante");
+
+            double salario;
+            salario = 1220.70;
+
+            Console.WriteLine(salario);
+
+            double idade;
+            idade = 15.0 / 2;
+
+            Console.WriteLine(idade);
+
+            idade = 5 / 3;
+            Console.WriteLine("5 / 3 = " + idade);
+
+            idade = 5.0 / 3;
+            Console.WriteLine("5.0 / 3 = " + idade);
+
+            Console.WriteLine("A execução acabou. Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
